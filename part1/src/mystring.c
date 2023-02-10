741153eb7948d9c2a0a0ef0b0f2e6cfc3ab168db
Emmanuel Gacel <evg2110@columbia.edu>
2023-02-10 13:41:04 -0500

    Built the 3 functions for mystring.c, under part1

--- part1/src/mystring.c
+++ part1/src/mystring.c
@@ -1,3 +1,27 @@
 #include "mystring.h"
 #include <stddef.h>
+//Taken from page 99 of the K&R textbook
+size_t my_strlen(char *src){
+	int n;
+	for (n = 0; *src != NULL; s++){
+		n++;
+	}
+	return n;
+}
 
+//Taken from page 105 of the K&R textbook
+char *my_strcpy(char *dst, char *src){
+	int *pdst = &dst[i];
+	if (*src != null){
+		while ((*dst++ = *src++) != NULL);
+		return pdst;
+
+	} else return NULL;
+}
+
+void my_strlower(char *src){
+	while(*src != NULL){
+		*src = tolower(*src);
+		src++;
+		}
+}
