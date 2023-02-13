429aeba2b1cb2b3eb261cf13da853d49139453a6
Emmanuel Gacel <evg2110@columbia.edu>
2023-02-12 20:42:47 -0500

    Added the function shells for linked_list.c, the arrow (->) grammar needs to be checked.

--- part2/src/linked_list.c
+++ part2/src/linked_list.c
@@ -21,3 +21,40 @@ void print_list(linked_list *list, void (*print_function)(void*)) {
                          (void)printf("NULL");
     printf("}\n\n");
 }
+
+linked_list* create_linked_list(){
+	linked_list *l = (linked_list *)malloc(sizeof*(inked_list));
+	if (linked_list != NULL){
+		l->head = NULL;
+		l->tail = NULL;
+		l->size_t = 0;
+	}
+	return n;
+}
+
+void insert_in_order(linked_list *list, void *data , int (*cmp)(const void*, const void *)){
+	if (list != null){
+		if (cmp(*data, list -> head) < 1){//Does this compare pointers or literals?
+			list -> head -> previous = *data;
+			list -> head = *data; //Are we storing literals of pointers?
+		}else if (cmp(*data, list -> tail) < 1){
+			list -> tail -> next = *data;//Check arrow grammar
+			list->tail = *data;
+		}else{
+			*void index;
+			for(index = (list -> head); cmp(*data, index) < 1; index = (index -> next));//Chech arrow grammar
+			index -> previous = *data;
+			index = (index -> previous);
+			index -> next = *data;
+			}
+		}
+	}else(cmp(*data, list -> tail) < 1, list - ){
+		list->head = list ->tail = *data;
+	}
+}
+
+void free_list(linked_list *list, void (*free_data)(void *)){
+	while((*list != null) != NULL){
+		free_data(list -> tail);
+	}
+}
