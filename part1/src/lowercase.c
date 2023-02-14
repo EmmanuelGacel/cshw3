2049ddd7b75592e4a24f142139881c9608ae6cfb
katiewimmer <71279410+katiewimmer@users.noreply.github.com>
2023-02-13 19:52:55 -0500

    completed part 1

--- part1/src/lowercase.c
+++ part1/src/lowercase.c
@@ -26,61 +26,27 @@ void display_strings(char **strings) {
 static char **copy_args_lowercase(int argc, char **argv) {
 	
 	if (argc > 1){
-		/**
-		int i; //skip first arg
-		size_t  count;
-
-		for (i = 1; i < argc; i++){ //saves in count, total # of chars
-			count = count + my_strlen(argv[i]); // *argv + i, goes to address of next str
-			count = count - 1; //takes out the ./lowercase and the last added space
-
-		}
-		
-		char **copy_pointer;
-		char *copy = (char*)malloc((count*sizeof(char) + 1)); //allocates memory based on count
-		
-		for (i = 1; i < argc; i++){ //copies over all the args into copy_pointer
-			printf("strcopy = %s\n", argv[i]);
-			my_strcpy(copy,(argv[i])); //passes array of string to cp
-		}
-
-
-		my_strlower(copy); //sets all values in cp to lowercase
-
-		//printf("copy = %s\n", copy_pointer[3]);
-		//printf("final: %s\n ", *copy_pointer);
-		copy_pointer = &copy;
-
-		return copy_pointer;
-		*/
-		
-		//MOST SUCCESSFUL VERSION
-		printf("made it 1!!! \n");
-		int d = (argc + 1) * sizeof(char*);
-		printf("argc + 1 = %d\n", (argc + 1));
-		printf("size of char = %ld\n", sizeof(char*));
-		printf("mallocked space: %d\n", d);
-		char **copy_pointer = (char**)(malloc((argc + 1) *sizeof(char*))); //allocates sufficient memory
-              
+		char **copy_pointer = (char**)(malloc(argc *sizeof(char*) + 1)); //allocates sufficient memory
+                
+                int num_args = argc; //saves value of args      
                 int length;
-                while(--argc > 0){
-
+                while(--num_args > 0){
                         length = my_strlen(*++argv);//length of current argument
                         printf("length = %d\n", length);
-			
-                        char temp[length + 1], *ptemp; //makes dst array with proper size
-                        ptemp = temp; //pointer to temp                 
-			
+             
+                        char *ptemp  = (char *)(malloc (length * sizeof(char) + 1)); //char array space
 
                         *++copy_pointer = (my_strcpy(ptemp, *argv)); //sets next pointer in cp
                         printf("Current cp = %s\n", *copy_pointer);
-			
+                        
+    
                         my_strlower(*copy_pointer); //lowers array cp is currently at   
                         printf("lowered cp = %s\n", *copy_pointer);
-
+                          
                 }
+                printf("argc = %d\n", argc);
 
-                return copy_pointer;
+                return copy_pointer - (argc - 2);
 		
 	}
 	return NULL;
