cd1ffd6d41cfd4a171da760622cacf1f3b61110b
Emmanuel Gacel <evg2110@columbia.edu>
2023-02-14 22:45:32 -0500

    Handled all of the error statements for part 1, A

--- part2/src/linked_list.c
+++ part2/src/linked_list.c
@@ -23,38 +23,41 @@ void print_list(linked_list *list, void (*print_function)(void*)) {
 }
 
 linked_list* create_linked_list(){
-	linked_list *l = (linked_list *)malloc(sizeof*(inked_list));
-	if (linked_list != NULL){
-		l->head = NULL;
-		l->tail = NULL;
-		l->size_t = 0;
-	}
-	return n;
+        linked_list *l = (linked_list *)malloc(sizeof(linked_list));
+        if (l != NULL){//Do we need this???
+                l->head = NULL;
+                l->tail = NULL;
+                l->size = 0;
+        }
+        return l;
 }
 
-void insert_in_order(linked_list *list, void *data , int (*cmp)(const void*, const void *)){
-	if (list != null){
-		if (cmp(*data, list -> head) < 1){//Does this compare pointers or literals?
-			list -> head -> previous = *data;
-			list -> head = *data; //Are we storing literals of pointers?
-		}else if (cmp(*data, list -> tail) < 1){
-			list -> tail -> next = *data;//Check arrow grammar
-			list->tail = *data;
-		}else{
-			*void index;
-			for(index = (list -> head); cmp(*data, index) < 1; index = (index -> next));//Chech arrow grammar
-			index -> previous = *data;
-			index = (index -> previous);
-			index -> next = *data;
-			}
-		}
-	}else(cmp(*data, list -> tail) < 1, list - ){
-		list->head = list ->tail = *data;
-	}
+bool insert_in_order(linked_list *list, void *data , int (*cmp)(const void*, const void *)){
+        node *n = create_node(data); //Is it okay if I assign this node to a pointer
+        if (list != NULL){
+                if (cmp(data, list -> head) < 1){//Does this compare pointers or literals?
+                        *list -> head -> prev = *n;
+                        *list -> head = *n; //Are we storing literals of pointers?
+                }else if (cmp(data, list -> tail) > 1){
+                        *list -> tail -> next = *n;//Check arrow grammar
+                        *list -> tail = *n;
+                }else{
+                        node* index;
+                        for(index = (list -> head); (cmp(data, index->data) > 0); index = (index -> next));//Chech arrow grammar
+                        *index -> prev =  *n;
+                        *index = (*index -> prev);
+                        *index -> next =  *n;
+                }
+        }else{
+                list->head = list ->tail =  n;
+        }
+        return true;
+
 }
 
 void free_list(linked_list *list, void (*free_data)(void *)){
-	while((*list != null) != NULL){
-		free_data(list -> tail);
-	}
+        while(list != NULL){
+                free_node(list -> tail, *free_data);
+        }
+        free_data(list);
 }
