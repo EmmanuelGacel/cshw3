1ecc79ac6eee24e86a4be9840ab9f30dd5f0931a
Emmanuel Gacel <evg2110@columbia.edu>
2023-02-16 17:50:42 -0500

    Bult the prototypes of the neccessary methods

--- part2/src/sortargs.c
+++ part2/src/sortargs.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include "linked_list.h"
-
+int str_cmp(const void *str1, const void *str2){
+	return strcmp( ( const char *)str1,  (const char *)str2);
+}
+void print_function (void* data){
+	printf("%s", (char*) data);
+}
 int main(int argc, char **argv) {
-    return EXIT_SUCCESS;
+	linked_list *list = create_linked_list();
+	int i;
+	for (i = 1; i < argc; i++){
+		insert_in_order(list, argv[i], str_cmp); 
+	}
+	print_list(list, print_function);
+	return EXIT_SUCCESS;
 }
