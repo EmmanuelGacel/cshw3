7081c1c06011b91070829f05472324712e925dcf
katiewimmer <krwim78@gmail.com>
2023-02-19 01:57:53 -0500

    hw3 completed

--- part2/src/linked_list.c
+++ part2/src/linked_list.c
@@ -87,111 +87,18 @@ bool insert_in_order(linked_list *list, void *data , int (*cmp)(const void*, con
 		}
 	}
         return true;
-	//printf("n -> data: %s \n",(char *)  n -> data);
-                 //printf("head -> data: %s\n", (char *) list -> head -> data);
-                 //printf("cmp data: %d\n",cmp(n->data, list -> head-> data));
-                 //printf("cmp data: %d\n",cmp(list -> head-> data, n -> data));
-		
-	  //printf("------------------------------- \n");
-                                //printf("Head -> data: %s\n", (char *) list -> head -> data);
-                                //printf("Tail -> data: %s\n", (char *) list -> tail -> data);
-                                //printf("Data: %s\n", (char *) data);
-                                //printf("Index -> data: %s\n", (char *) index -> data);
-                                //printf("cmp data: %d\n",cmp(data, index->data));
-                                //printf("------------------------------- \n");
-                                //printf("cmp data: %d\n",cmp(data, index->data));
-
-			/*
-                        if(cmp(data, index->data) == 0){ //case for duplicates
-                                printf("323Index -> data: %s\n", (char *) index -> data);
-                                while(cmp(data, index->data) == 0 && index != NULL){
-                                        index = (index->next);//move past all duplicates
-                                }
-                                printf("Made it 2 \n");
-                                if(index->next != list->tail){
-                                        printf("Made it 3 \n");
-                                        n->next = index->next; //sets n's next to node after index
-                                        index->prev->next = n; //sets node before index's next to n
-                                        ++list->size;
-                                }
-                                else{
-                                        list->tail->next = n; //sets node after the tail to
-                                        list -> tail = n;
-                                        ++list->size;
-
-                                }
-                        */
-
-				
-				/*
-				if (index -> next != NULL){
-					printf("How did we get here\n");
-					n -> prev = index -> prev;
-					n -> next = index;
-					//printf("Index -> data: %s\n", (char *) index -> data); 
-					index = n -> prev;
-					//printf("Index -> data: %s\n", (char *) index -> data);
-					//printf("Made it 4\n");
-					//printf("Index -> next: %p", index);
-					index -> next = n;
-					//printf("Made it 5\n");
-					++list->size;
-				}else{
-					//:if (list -> head != list -> tail)
-					printf("Last Tail Assignment:\n");
-					//printf("Made it 7\n");
-					//printf("Made it 8\n");
-					printf ("cmp( data, tail) : %d \n",cmp(data, list -> tail -> data));
-					printf("n -> data: %s\n", (char *) data);
-					printf("Index -> data: %s\n", (char *) index -> data);
-					printf("Tail -> data: %s\n", (char *) list -> tail -> data);
-					list -> tail -> next = n;
-					n -> prev = list -> tail;
-					list -> tail = n;
-					//printf("Tail -> data: %s\n", (char *) list -> tail -> data);
-					//index -> next = n;
-					//index = index -> next;
-					//printf("Index -> data: %s\n", (char *) index -> data);
-					//printf("Made it 7\n");
-					++list->size;
-					////n -> prev = index;
 					
-					//index -> next = n;
-					
-					////list -> tail = n;
-					
-						//index -> next = n;
-						//n -> prev = index;
-						//list -> tial - n;
-				
-				}
-
-			}
-			*/
-			/*else{ //node is equal to head
-				node* index = list->head; //creates temp pointer to head
-                        	while(cmp(data, index->data) == 0 && (index -> next) != NULL){
-                                	index = (index->next);//move past all duplicates
-                        	}
-                        if(index->next != list->tail){
-					n->next = index->next; //sets n's next to node after index
-                                	index->prev->next = n; //sets node before index's next to n
-                        	}
-                        else{	
-                                	list->tail->next = n; //sets node after the tail to
-                                	list -> tail = n;
-                        	}*/
-               
-                
-        //else{ //list does not yet exist (set head and tail to n)
-                //list->head = list->tail = n;
-        //}
-	
 }
 
 void free_list(linked_list *list, void (*free_data)(void *)){
-        while(list != NULL){
-                free_node(list -> tail, *free_data);
-        }
-        free_data(list);
+
+	node *iterator = list->tail; //starts at the tail
+	
+	while(iterator != list->head){ //frees each node starting at the tail
+                free_node(iterator, *free_data); //frees the individual node
+		iterator = iterator->prev; //increments iterator
+	}
+	free_node(iterator, *free_data); //frees the head, since iterator now is equal to head
+					 //
+	free(list);//frees the linked list
 }
--- part2/src/sortargs.c
+++ part2/src/sortargs.c
@@ -9,12 +9,19 @@ int str_cmp(const void *str1, const void *str2){
 void print_function (void* data){
 	printf("%s", (char*) data);
 }
+void free_data(void *data) {
+	//char *str = (char *)data;
+    	//free(str);
+}
+
 int main(int argc, char **argv) {
 	linked_list *list = create_linked_list();
 	int i;
 	for (i = 1; i < argc; i++){
 		insert_in_order(list, argv[i],str_cmp); 
 	}
-	print_list(list, print_function);
+	print_list(list, print_function); //prints out the list
+	free_list(list, free_data);//frees the allocated memory
 	return EXIT_SUCCESS;
 }
+
